8f3616f3d52dd63a16aa6894fb571346d9541581
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2024-07-04 12:10:07 +0530

    Revert "Update App.jsx"
    This reverts commit 1d944091d7c2d01d1bdf34796beb58b98690e275.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
-import { usePDF } from "react-to-pdf";
 
 function App() {
-  const { toPDF, targetRef } = usePDF({ filename: "page.pdf" });
   return (
-    <div ref={targetRef}>
+    <>
       <Navbar></Navbar>
       <Form></Form>
-      <center>
-        <button className="btn btn-dark" onClick={() => toPDF()}>
-          {" "}
-          Download
-        </button>
-      </center>
-    </div>
+    </>
   );
 }
 
