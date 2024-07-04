1d944091d7c2d01d1bdf34796beb58b98690e275
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2024-07-04 12:07:12 +0530

    Update App.jsx
    Added a download button to download the Todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
+import { usePDF } from "react-to-pdf";
 
 function App() {
+  const { toPDF, targetRef } = usePDF({ filename: "page.pdf" });
   return (
-    <>
+    <div ref={targetRef}>
       <Navbar></Navbar>
       <Form></Form>
-    </>
+      <center>
+        <button className="btn btn-dark" onClick={() => toPDF()}>
+          {" "}
+          Download
+        </button>
+      </center>
+    </div>
   );
 }
 
